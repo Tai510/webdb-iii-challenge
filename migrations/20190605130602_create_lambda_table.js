c201cc0d115b86f6a6cd7acb6bc871f13e81a6fc
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-05 13:54:59 -0700

    maigration and seeds files created

--- migrations/20190605130602_create_lambda_table.js
+++ migrations/20190605130602_create_lambda_table.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('cohorts', function(tbl) {
+        // we must use the callback syntax for .createTable()
+        tbl.increments(); // pass the name if you wanted to be called anything other than id
+        tbl
+          .string('name', 255)
+          .notNullable()
+          .unique();
+        tbl.timestamp(true, true);
+      });
+ 
+ };
+ 
+ exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('cohorts');
+ };
